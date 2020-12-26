bf75b8b862d6809a621528a8e9f79702657166c9
Ulyana224 <76625398+Ulyana224@users.noreply.github.com>
2020-12-26 21:43:07 +0400

    Add files via upload

---
+++ 8.js
@@ -0,0 +1,6 @@
+function show (name)
+{
+	var elem = document.getElementById(name);
+	if(elem)
+	elem.style.display = "block";
+}
---
+++ 9.js
@@ -0,0 +1,6 @@
+function check()
+{
+if ( question.answer.value == "Тема поэта и поэзии" )
+	alert ("Верно)");
+else alert ("Неверно(");
+}
